ec1ca526a87802c9eb4eaa24a662cbb6087da2ce
canarioario <cucutequiero2@gmail.com>
2021-10-11 17:56:10 +0200

    v1

--- ej1.php
+++ ej1.php
@@ -1,7 +1,14 @@
-*/
+<?php
+/* 
 Ejercicio 1. Defina dos variables cuyos nombres sean “uno” y “dos” e imprímelas por pantalla.
 Ponga un comentario con el tipo de dato que contienen.
-/*
-<?php
+*/
+
+// declarar variables 
+ $a = 10;
+ $b = 25;
+// mostrar variables
+echo  "La variable 1 es igual =  $a <br>";
+echo  "La variable 2 es igual =  $b ";
 
 ?>
