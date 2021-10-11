7d12d5b1a355269d93cd4dbdad10a9cfa268826e
canarioario <cucutequiero2@gmail.com>
2021-10-11 18:20:13 +0200

    v2

---
+++ ej2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Ejercicio 2. Escribe un programa que imprima por pantalla los cuadrados
+(el número multiplicado por sí mismo) de los 30 primeros números naturales.
+*/
+
+for ($a = 1; $a<=30; $a++) {
+
+    echo $a * $a; //  imprimir por pantalla los cuadrados
+
+    echo "<br />";
+
+}
+
+
+
+?>
