7ca0cc8129f958a38eddc8c8400fd84db5687ec5
canarioario <cucutequiero2@gmail.com>
2021-10-12 19:00:28 +0200

    v2

--- ej5.php
+++ ej5.php
@@ -3,7 +3,7 @@
 /*
 Imprimir por pantalla la tabla de multiplicar del número pasado en un parámetro GET por la URL.
 */
-$numero = $_REQUEST["numero"];
+$numero =$_REQUEST['numero'];
 
 for ($i=1; $i <11; $i++) { 
     
