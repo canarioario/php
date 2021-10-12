d238f6737ed2250c9099ad9f6c78b0989d04e8ed
canarioario <cucutequiero2@gmail.com>
2021-10-12 18:33:29 +0200

    v1
    tabla de multiplicar con el num introducido (for)

---
+++ ej5.php
@@ -0,0 +1,19 @@
+
+<?php
+/*
+Imprimir por pantalla la tabla de multiplicar del número pasado en un parámetro GET por la URL.
+*/
+$numero = $_REQUEST["numero"];
+
+for ($i=1; $i <11; $i++) { 
+    
+     
+    echo $numero. " x ". $i.  " = ".($numero * $i)."<br/>";
+    
+
+}
+
+?>
+
+
+
