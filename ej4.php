8768d9f4cd1dd28832cb3b3a0438ea02397bb528
canarioario <cucutequiero2@gmail.com>
2021-10-12 18:33:37 +0200

    Update ej4.php

---
+++ ej4.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Ejercicio 4. Escribe un programa que multiplique los 20 primeros números naturales.*/
+
+for ($i=1; $i <21; $i++) { 
+    
+     
+    echo $i. " x ". $i.  " = ".($i * $i)."<br/>";
+    
+
+}
+
+
+?>
