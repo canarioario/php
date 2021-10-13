ca0e61bd6ddb5ccff3c24c438df6fc6877cbc537
canarioario <cucutequiero2@gmail.com>
2021-10-13 12:42:10 +0200

    v4

--- ej7.php
+++ ej7.php
@@ -5,17 +5,17 @@ Ejercicio 7. Igual que el anterior pero utilizando el foreach.
 */
 echo "-------------------------------INICIO-----------------------------------------------"."<br/>";
  $meses = array('enero',
-'febrero',
-'marzo',
-'abril',
-'mayo',
-'junio',
-'julio',
-'agosto',
-'septiembre',
-'octubre',
-'noviembre',
-'diciembre');
+                'febrero',
+                'marzo',
+                'abril',
+                'mayo',
+                'junio',
+                'julio',
+                'agosto',
+                'septiembre',
+                'octubre',
+                'noviembre',
+                'diciembre');
 
 foreach ($meses as $mes) {
     echo " $mes<br/>";
