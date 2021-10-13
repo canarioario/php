2af8b06c05d37cb6b5749ab66326e7de0bde2f92
canarioario <cucutequiero2@gmail.com>
2021-10-13 12:36:01 +0200

    v2

--- ej6.php
+++ ej6.php
@@ -4,6 +4,16 @@ Crear un array llamado meses y que almacene el nombre de los doce meses del año
 Recorrerlo con FOR para mostrar por pantalla los doce nombres.
 */
 
+echo "-------------------------------INICIO-----------------------------------------------"."<br/>";
+$meses = array('enero',
+ 'febrero',
+  'marzo', 'mayo','junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre');
+
+
+for ($i=0; $i < count($meses); $i++) {
+    echo " $meses[$i]<br/>";
+}
+echo "<br />-------------------------------FIN-----------------------------------------------";
 
 
 
