3f70905a2bea42652597ca884394d4c2c15e4de9
canarioario <cucutequiero2@gmail.com>
2021-10-13 12:18:31 +0200

    v1

---
+++ ej7.php
@@ -0,0 +1,9 @@
+<?php
+echo "-------------------------------INICIO-----------------------------------------------"."<br/>";
+$meses = array('enero', 'febrero', 'marzo', 'mayo','junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre');
+
+foreach ($meses as $mes) {
+    echo " $mes<br/>";
+}
+echo "<br />-------------------------------FIN-----------------------------------------------";
+?>
