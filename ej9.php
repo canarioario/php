f57827f5b4048df69b87f7485b242100dbc82e98
canarioario <cucutequiero2@gmail.com>
2021-10-13 18:54:42 +0200

    Update ej9.php

--- ej9.php
+++ ej9.php
@@ -2,7 +2,19 @@
 /*
 Ejercicio 9. Mostrar todos los números pares que hay entre el 1 y el 100.
 */
+echo "-------------------------------INICIO-----------------------------------------------";
+
+for ($a = 1; $a<=100; $a++) {
+
+       $i= $a%2==0
 
 
+    
+      //  imprimir pares
+
+      echo "<br />Los numeros pares del 1 al 100 son  ";
+
+}
+echo "<br />-------------------------------FIN-----------------------------------------------";
 
 ?>
