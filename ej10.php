1ecb06ced13d4fd82d21afd24b5c77e9f450b5ec
canarioario <cucutequiero2@gmail.com>
2021-10-15 13:50:26 +0200

    v1

--- ej10.php
+++ ej10.php
@@ -3,7 +3,20 @@
 Ejercicio 10. Mostrar los números múltiplos de un número pasado por la URL que
 hay del 1 al 100.
 */
+echo "-------------------------------INICIO-----------------------------------------------";
 
 
+for ($i=1; $i <=  100; $i++) { 
+   
+if( isset($_GET["numero"]) && $i%$_GET["numero"] == 0){
+
+	echo " <br />"  . $i . " es multiplo de " .$_GET["numero"];
+   }
+}
+echo "<br />-------------------------------FIN-----------------------------------------------";
+
+
+// añadir codigo del video
 
 ?>
+
