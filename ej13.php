eb802dc6b36cfc321066233c7a2627756a4daeda
canarioario <cucutequiero2@gmail.com>
2021-10-15 19:50:43 +0200

    v1

--- ej13.php
+++ ej13.php
@@ -4,6 +4,18 @@ Ejercicio 13. Escribe un programa que muestre la dirección IP del usuario que
 visita nuestra web y si usa Firefox darle la enhorabuena.
 */
 
+$ip = $_SERVER["REMOTE_ADDR"]; 
+$browser =$_SERVER["HTTP_USER_AGENT"];
+
+echo "Tu ip es " . $ip;
+
+if(strstr($browser,"Firefox") == true){
+
+   echo "<br/>El navegador que usas es Firefox Enorabuena";
+
+} else {
+    echo "<br/>no usas firefox";
+}
 
 
 ?>
