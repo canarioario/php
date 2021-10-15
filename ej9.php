6af7b7ab0aa4fe0bf80118ff540da07e56977d86
canarioario <cucutequiero2@gmail.com>
2021-10-15 13:54:05 +0200

    Update ej9.php

--- ej9.php
+++ ej9.php
@@ -3,7 +3,7 @@
 Ejercicio 9. Mostrar todos los números pares que hay entre el 1 y el 100.
 */
 echo "-------------------------------INICIO-----------------------------------------------";
-
+// para saber los impares el if seria asi: ($a%2!=0)
 for ($a = 1; $a<=100; $a++) {
       if ($a%2==0) {
             echo "<br />Los numeros pares del 1 al 100 son:  " . $a;
