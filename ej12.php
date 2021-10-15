a12043db7f5d9608ae13ae9d812b6a7e167cd298
canarioario <cucutequiero2@gmail.com>
2021-10-15 19:40:17 +0200

    v1

--- ej12.php
+++ ej12.php
@@ -7,7 +7,25 @@ lo siguiente con él:
 3. Mostrará su longitud.
 4. Buscar en el vector.
 */
+$numeros = array('80','20','40','10','50' );
+echo "<h2>Recorrer y mostrar</h2>";
+foreach ($numeros as $num){
+    echo $num."<br/>";
+}
+echo "<h2>Recorrerlo y mostrarlo</h2>";
+sort($numeros); //sort para ordenar
+foreach ($numeros as $num){
+    echo $num."<br/>";
+}
 
+echo "<h2>Mostrara su longitud</h2>" .count($numeros); // o .sizeof
+count($numeros);
+echo "<h2>Buscar en el vector</h2>";
+if (!array_search(50,$numeros)) { 
+    echo "El numero no exite en el array";
+}else {
 
+    echo "el numero si exite" ;
+}
 
 ?>
