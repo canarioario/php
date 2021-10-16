64d241ac858d50ef792231dd88a61db4f163678f
canarioario <cucutequiero2@gmail.com>
2021-10-16 18:19:41 +0200

    v1

--- ej22.php
+++ ej22.php
@@ -3,6 +3,13 @@
 Ejercicio 22. Implemente la función filter_var para comprobar si el email que nos llega por la URL es un email valido.
 */
 
+$email= $_GET[""];
 
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email es un correo electronico valido");
+  } else {
+    echo("$email no es correo electronico valido");
+  }   
 
 ?>
