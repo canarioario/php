09a20208267f194f5c2d7b476efc2935c39d35d7
canarioario <cucutequiero2@gmail.com>
2021-10-16 17:40:20 +0200

    v1

--- ej19.php
+++ ej19.php
@@ -3,6 +3,14 @@
 Ejercicio 19. Escriba un programa que calcule la factorial de un número que se le pase mediante un parámetro POST enviado desde un formulario
 */
 
+$factorial=1 ;
+$numero = $_POST["numero"];
 
-
+for ($i=1; $i <= $numero; $i++) { 
+   //  $factorial =   $factorial * $i;
+    $factorial *= $i;
+  
+}
+echo "El factorial de ". $numero . " es ". $factorial ;
+    
 ?>
