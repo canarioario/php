efe1f8e2f2f5bc3998dff0a1ded4a9e492e35bfd
canarioario <cucutequiero2@gmail.com>
2021-10-16 13:36:01 +0200

    v2

--- ej4.php
+++ ej4.php
@@ -1,14 +1,15 @@
+<meta charset= "utf-8">
 <?php
 /*
 Ejercicio 4. Escribe un programa que multiplique los 20 primeros números naturales.*/
-
-for ($i=1; $i <21; $i++) { 
-    
-     
-    echo $i. " x ". $i.  " = ".($i * $i)."<br/>";
-    
-
+$contador =2;
+$numero = 1;
+while ($contador <= 20) {
+    $numero *= $contador;
+    echo $numero . "<br/>"; // para ver como se va multplicando 
+    $contador++;
 }
-
+   
+echo "El resultado de multiplicar los 20 primero numeros es: " . $numero;
 
 ?>
