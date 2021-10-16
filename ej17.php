b0173628db3c383d4f9242e0d38b7e044823f058
canarioario <cucutequiero2@gmail.com>
2021-10-16 13:03:49 +0200

    v1

--- ej17.php
+++ ej17.php
@@ -3,6 +3,19 @@
 Ejercicio 17. Crea un script PHP que tenga tres variables, una tipo array, otra tipo
 string y otra boleana y que imprima un mensaje según el tipo de variable que sea.
 */
+$array1 = array("hola", 2 );
+$texto = "Soy string";
+$bol = true;
 
+if (is_array($array1)) {
+    echo "El array es un array"; 
+}
+
+if (is_bool($bol)) {
+    echo "<br/>El booleano es un booleano"; 
+}
+if (is_string($texto)) {
+    echo "<br/>El string es un string";
+}
 
 ?>
