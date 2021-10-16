c12a806317a29767221febf728951a255c4eecb7
canarioario <cucutequiero2@gmail.com>
2021-10-16 18:14:55 +0200

    ejercicio21.v1

--- app/index.php
+++ app/index.php
@@ -3,5 +3,8 @@ include 'includes/header.php'
 ?>
 <p>Texto del body en el index.</p>
 <?php
+/*
+Ejercicio 21. Implemente los includes de PHP para tener una estructura html básica y separar el código por el header, body y footer.
+*/
 include 'includes/footer.php'
 ?>
--- ej21.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Ejercicio 21. Implemente los includes de PHP para tener una estructura html básica y separar el código por el header, body y footer.
-*/
-
-
-
-?>
