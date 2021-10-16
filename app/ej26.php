1db4f6d10d8202e50422221bec8052efdee798a3
canarioario <cucutequiero2@gmail.com>
2021-10-16 23:20:53 +0200

    Update ej26.php

--- app/ej26.php
+++ app/ej26.php
@@ -8,24 +8,36 @@ en el caso de que existan y tengan un valor asociado, o un mensaje 'No definido 
 if (isset($_POST["submit"])) {
    if (!empty($_POST["nombre"])) {
       echo $_POST["nombre"]."<br/>";
+   }else {
+      echo "Rellene el nombre"."<br/>";
    }
    if (!empty($_POST["apellidos"])) {
     echo $_POST["apellidos"]."<br/>";
- }
+ }else {
+   echo "Rellene los apellidos"."<br/>";
+}
  if (!empty($_POST["bio"])) {
     echo $_POST["bio"]."<br/>";
- }
+ }else {
+   echo "Rellene la biografia"."<br/>";
+}
  if (!empty($_POST["email"])) {
     echo $_POST["email"]."<br/>";
- }
+ }else {
+   echo "Rellene el email"."<br/>";
+}
  if (!empty($_POST["image"])) {
     echo $_POST["image"]."<br/>";
- }
- if (!empty($_POST["password"])) {
-    echo $_POST["password"]."<br/>";
- }
-    
+ }else {
+   echo "Rellene el image"."<br/>";
 }
+ if (!empty($_POST["password"])) {
+    echo sha1 ($_POST["password"])."<br/>";
+ }else {
+   echo "Rellene la password"."<br/>";
+}
+    
+} 
 
 
 
