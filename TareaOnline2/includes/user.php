d2cfad11371723e3353beea318dd756c82ed85fe
canarioario <cucutequiero2@gmail.com>
2022-01-05 18:47:48 +0100

    image

--- TareaOnline2/includes/user.php
+++ TareaOnline2/includes/user.php
@@ -2,123 +2,114 @@
 require_once 'config.php';
 //var_dump($_POST);
 // Mensaje que indicará al usuario si la inserción se realizó correctamente o no
-$msgresultado ="";
+   $msgresultado ="";
+// Array asociativo que almacenará los mensajes de error que se generen por cada campo
+   $errores= array();
 // Si se ha pulsado el botón guardar...
-if(isset($_POST['submit'])){ // y hermos recibido las variables del formulario y éstas no están vacías...
-  if(isset($_POST) and (!empty($_POST))){
-      if (!empty($_POST["name"]) && (!preg_match("/[0-9]/", $_POST["name"])) && (strlen($_POST["name"]) < 20)) {
-        $nombre = trim($_POST["name"]);
-        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
-      } else {
-        $errors["name"] = "El nombre introducido no es válido :(";
-      }
-      if (!empty($_POST["email"])) {
-        $correo = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-        if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
-          $errors["email"] = "La dirección email introducida no es válida :(";
+if(isset($_POST['submit']))
+ { // y hermos recibido las variables del formulario y éstas no están vacías...
+  if(isset($_POST) and (!empty($_POST)))
+   {
+    $nombre   = $_POST['txtnombre'];
+    $password = sha1($_POST['txtpass']);
+    $email    = $_POST['txtemail'];
+    /* Realizamos la carga de la imagen en el servidor */
+    // Hacemos un var_dump ara comprobar la estructura de la variable $_FILES["imagen"]
+    //var_dump($_FILES["imagen"]);
+    // Comprobamos que el campo tmp_name tiene un valor asignado para asegurar que hemos
+    //recibido la imagen correctamente
+
+    // Definimos la variable $imagen que almacenará el nombre de imagen 
+    // que almacenará la Base de Datos inicializada a NULL
+    $imagen = NULL;
+
+    if(isset($_FILES["imagen"]) && (!empty($_FILES["imagen"]["tmp_name"]))){
+    // Verificamos la carga de la imagen
+     var_dump($_FILES["imagen"]);
+    // die();
+
+    // Comprobamos si existe el directorio fotos, y si no, lo creamos
+       if(!is_dir("fotos"))
+         { $dir = mkdir("fotos", 0777, true);  }
+        else{ $dir = true; }
+    // Ya verificado que la carpeta uploads existe movemos el fichero seleccionado a dicha carpeta
+       if($dir){
+          //Para asegurarnos que el nombre va a ser único...
+          $nombrefichimg = time() . "-" . $_FILES["imagen"]["name"];
+          // Movemos el fichero de la carpeta temportal a la nuestra
+          $movfichimg    = move_uploaded_file($_FILES["imagen"]["tmp_name"],"fotos/".$nombrefichimg);
+          $imagen = $nombrefichimg;
+          // Verficamos que la carga se ha realizado correctamente
+          if($movfichimg){
+                  $imagencargada = true;
+          }  else {
+                  $imagencargada = false;
+                  $errores["imagen"]="Error: La imagen no se cargó correctamente! :(";
+          }
         }
-      }
-      if (!empty($_POST["password"]) && (strlen($_POST["password"]) > 6) && (strlen($_POST["password"]) <= 10)) {
-        $password = sha1($_POST["password"]);
-      } else {
-        $errors["password"] = "Introduzca una contraseña válida (6-10 caracteres) :(";
-      }
-    $nombre = $_POST['name'];
-    $password = sha1($_POST['password']);
-    $email = $_POST['email'];
-    try{  //Definimos la instrucción SQL parametrizada 
-        $sql = "INSERT INTO usuarios(nombre,password,email)
-                       VALUES (:nombre,:password,:email)";
-        // Preparamos la consulta...
-        $query = $conexion->prepare($sql); 
-        // y la ejecutamos indicando los valores que tendría cada parámetro
-        $query->execute ([
-            'nombre'   => $nombre,
-            'password' => $password,
-            'email'    => $email
-        ]); //Supervisamos si la inserción se realizó correctamente... 
-        if($query){
-          $msgresultado = '<div class="alert alert-success">' . "El usuarios se registró correctamente!! :)" . '</div>';
-        }// o no :(
-      }catch (PDOException $ex){
-          $msgresultado = '<div class="alert alert-success">' . "El usuario no pudo registrarse!! :(" . '</div>';
-          die();
-      }
+        var_dump($imagen);
+    }   
+    if(count($errores)==0){
+      try{  //Definimos la instrucción SQL parametrizada 
+          $sql = "INSERT INTO usuarios(nombre,password,email,imagen)
+                         VALUES (:nombre,:password,:email , :imagen)";
+          // Preparamos la consulta...
+          $query = $conexion->prepare($sql); 
+          // y la ejecutamos indicando los valores que tendría cada parámetro
+          $query->execute ([
+              'nombre'   => $nombre,
+              'password' => $password,
+              'email'    => $email,
+              'imagen'   => $imagen
+          ]); //Supervisamos si la inserción se realizó correctamente... 
+          if($query){
+            $msgresultado = '<div class="alert alert-success">' . "El usuarios se registró correctamente!! :)" . '</div>';
+          }// o no :(
+        }catch (PDOException $ex){
+            $msgresultado = '<div class="alert alert-danger">' . "El usuario no pudo registrarse!! :( (" . $ex->getMessage(). ')</div>';
+            die();
+        }
+    }else{
+      $msgresultado = '<div class="alert alert-danger">' . "Datos de registro de usuario erróneos!! :( (" . $ex->getMessage(). ')</div>';
+      die();
     }
+   }
   }
 ?>
-<?php
-/**
- * Script que muestra en una tabla los valores enviados por el usuario a través 
- * del formulario utilizando el método POST
- */
-// Definimos e inicializamos el array de errores y las variables asociadas a cada campo
-$errors    = [];
-$nombre    = "";
-$correo    = "";
-$password  = "";
-// Función que muestra el mensaje de error bajo el campo que no ha superado
-// el proceso de validación
-function mostrar_error($errors, $campo) {
-  $alert = "";
-  if (isset($errors[$campo]) && (!empty($campo))) {
-    
-    $alert = '<div class="alert alert-danger" style="margin-top:5px;">' . $errors[$campo] . '</div>';
-  }
-  return $alert;
-}
-// Verificamos si todos los campos han sido validados
-function validez($errors) {
-  if (isset($_POST["submit"]) && (count($errors) == 0)) {
-    return '<div class="alert alert-success" style="margin-top:5px;"> Formulario validado correctamente!! :)</div>';
-  }
-}
-// Visualización de las variables obtenidas mediante el formulario
-if (isset($_POST["submit"])) {
-  if (!empty($_POST["name"]) && (!preg_match("/[0-9]/", $_POST["name"])) && (strlen($_POST["name"]) < 20)) {
-    $nombre = trim($_POST["name"]);
-    $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
-  } else {
-    $errors["name"] = "El nombre introducido no es válido :(";
-  }
-  if (!empty($_POST["email"])) {
-    $correo = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
-      $errors["email"] = "La dirección email introducida no es válida :(";
-    }
-  }
-  if (!empty($_POST["password"]) && (strlen($_POST["password"]) > 6) && (strlen($_POST["password"]) <= 10)) {
-    $password = sha1($_POST["password"]);
-  } else {
-    $errors["password"] = "Introduzca una contraseña válida (6-10 caracteres) :(";
-  }
-}
-?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Base de Datos con PHP y PDO</title>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <!--    Referencia a la CDN de la hoja de estilos de Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
   <body class="cuerpo">
+    <div class="centrar">	
    <div class="container centrar">
-     <a href="addUser.php">Inicio</a>&nbsp&nbsp&nbsp&nbsp
-     <a href="includes\listado.php">Listado</a>
-     <div class="container cuerpo text-center centrar">	
+     <a href="addUser.php">Inicio</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+     <a href="includes/listado.php">Listado</a>
+     <div class="container cuerpo text-center centrar">	 
        <p><h2><img class="alineadoTextoImagen" src="images//user.png" width="50px"/>Añadir Usuario</h2> </p>
      </div>
      <?php echo $msgresultado;  ?> 
-     <form action="adduser.php" method="post">
-     <label for="name">Nombre:
-        <input type="text" name="name" class="form-control" <?php if(isset($_POST["name"])){echo "value='{$_POST["name"]}'";} ?> /> 
-        <?php echo mostrar_error($errors, "name"); ?>    
-      </label>
-      <br/>
-      <label for="email">Correo:
-        <input type="email" name="email" class="form-control" <?php if(isset($_POST["email"])){echo "value='{$_POST["email"]}'";} ?> /> 
-        <?php echo mostrar_error($errors, "email"); ?>                      
-      </label>
-      <br/>
-      <label for="password">Contraseña:
-        <input type="password" name="password" class="form-control" <?php if(isset($_POST["password"])){echo "value='{$_POST["password"]}'";} ?> />
-        <?php echo mostrar_error($errors, "password"); ?>                       
-      </label>
+     <form action="adduser.php" method="post" enctype="multipart/form-data">
+       <label for="txtnombre">Nombre
+       <input type="text" class="form-control" name="txtnombre" required></label>
        <br/>
+       <label for="txtemail">Email
+         <input type="email" class="form-control" name="txtemail"></label>
+       <br/>
+       <label for="txtpass">Contraseña
+       <input type="password" class="form-control" name="txtpass" required></label>
+       <br/>
+       <label for="imagen"> Imagen <input type="file" name="imagen" class="form-control" /></label>
+       </br>
        <input type="submit" value="Guardar" name="submit" class="btn btn-success">
      </form>
    </div>
   </body>
+</html>
